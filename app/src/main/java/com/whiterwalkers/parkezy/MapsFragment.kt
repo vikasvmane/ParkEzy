57ba903e23ad4c10f0699e484d0db14d4aea80a2
Bhagyashree <bhagyashreevm25@gmail.com>
2022-11-10 15:45:06 +0530

    Added location services

--- app/src/main/java/com/whiterwalkers/parkezy/MapsFragment.kt
+++ app/src/main/java/com/whiterwalkers/parkezy/MapsFragment.kt
@@ -1,11 +1,16 @@
 package com.whiterwalkers.parkezy
 
+import android.Manifest
+import android.location.Location
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -15,7 +20,8 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
 class MapsFragment : Fragment() {
-
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var googleMap: GoogleMap
     private val callback = OnMapReadyCallback { googleMap ->
         /**
          * Manipulates the map once available.
@@ -26,6 +32,7 @@ class MapsFragment : Fragment() {
          * install it inside the SupportMapFragment. This method will only be triggered once the
          * user has installed Google Play services and returned to the app.
          */
+        this.googleMap = googleMap
         val sydney = LatLng(-34.0, 151.0)
         googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
@@ -43,6 +50,54 @@ class MapsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
+        // Before you perform the actual permission request, check whether your app
+        // already has the permissions, and whether your app needs to show a permission
+        // rationale dialog. For more details, see Request permissions.
+        locationPermissionRequest.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        )
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(activity!!)
+        getLastLocation()
+    }
 
+    private val locationPermissionRequest = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        when {
+            permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                // Precise location access granted.
+                getLastLocation()
+            }
+            permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                // Only approximate location access granted.
+                getLastLocation()
+            }
+            else -> {
+                // No location access granted.
+            }
+        }
+    }
+
+    private fun getLastLocation() {
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location: Location? ->
+                setNewLocation(location)
+            }
+    }
+
+    private fun setNewLocation(location: Location?) {
+        location?.let {
+            if (this::googleMap.isInitialized) {
+                val currentLatLng = LatLng(it.latitude, it.longitude)
+                googleMap.clear()
+                googleMap.addMarker(
+                    MarkerOptions().position(currentLatLng).title("Marker in Current Location")
+                )
+                googleMap.moveCamera(CameraUpdateFactory.newLatLng(currentLatLng))
+            }
+        }
     }
 }
